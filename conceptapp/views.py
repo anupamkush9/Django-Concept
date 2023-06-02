7676dfc840b481c9079e60f6e32235b561fcf8c0
Anupamkush9 <anupam.kush9@gmail.com>
2023-06-02 23:39:54 +0530

    django-debug-toolbar-implemented

--- conceptapp/views.py
+++ conceptapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index_view(request):
+    print("====================")
+    import time
+    time.sleep(1)
+    return render(request, 'conceptapp/index.html',{"t1":"akshay"})
