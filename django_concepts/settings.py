e3c64a60201dee1c92cf79d1281039ebfa3fb11b
Anupamkush9 <anupam.kush9@gmail.com>
2023-06-03 12:58:04 +0530

    working with 2 databases-4

--- django_concepts/settings.py
+++ django_concepts/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'read_db': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3_read_db',
     }
 }
 
